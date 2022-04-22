db7b73095a7388545c1602e177c27650c7d871c5
adikarais <anandaadikarais200802@gmail.com>
2022-04-22 19:25:22 +0700

    input

--- studi kasus 5.cpp
+++ studi kasus 5.cpp
@@ -5,10 +5,23 @@ using namespace std;
 class Studi5{
   public:
 // Input
-
-
-
-
+  void input(){
+    //nama
+    cout<<"Masukkan nama mahasiswa = ";
+    cin>>nama;
+    //nim
+    cout<<"Masukkan NIM = ";
+    cin>>nim;
+    //daftar mata kuliah
+    cout<<"Masukkan banyak mata kuliah yang diambil= ";  cin>>jmlh_matakuliah;
+  for(int i=0;i<jmlh_matakuliah;i++){
+    cout<<"masukkan mata kuliah - "<< i+1<<" :"; cin>>matakuliah[i];
+  }
+    //jumlah sks
+    cout<<"masukkan jumlah sks = ";
+    cin>>sks;
+  }
+  
 //proses
   int proses(int sks){
     if(sks<1){
@@ -28,24 +41,7 @@ class Studi5{
   }
   
   //output
-  void output(){
-    
-    //NIM
-    cout << "NIM: " << nim << endl;
-    //Nama
-    cout << "Nama: " << nama << endl;
-    //Daftar Mata Kuliah
-    cout << "Mata Kuliah yang diambil:\n"; 
-      for(int i=0;i<jmlh_matakuliah;i++){
-        cout<<" mata kuliah - "<<i+1<<" ="<<matakuliah[i]<<endl;
-      }
-    //Jumlah SKS
-    cout << "Jumlah SKS: " << sks << endl;
-    //Total sebelum diskon
-    cout << "Total sebelum diskon: " << total << endl;
-    //Total Pembayaran setelah diskon
-    cout << "Total Pembayaran:  \t" << total_pembayaran << endl;
-  }
+
 
 
   private:
@@ -60,8 +56,7 @@ class Studi5{
 
 int main(void) {
   Studi5 s;
-  s.output();
-
+  s.input();
 
   return 0;
 }
